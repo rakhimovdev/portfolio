c89edf7e8dd5e72165a3e5026a48dbbe2d1823e3
Muhammadali <rahimov.dev1@gmail.com>
2024-01-18 12:57:19 +0500

    remove img

--- src/content_option.js
+++ src/content_option.js
@@ -89,28 +89,7 @@ const dataportfolio = [{
     img: img3,
     description: "The wisdom of life consists in the elimination of non-essentials.",
     link: "#",
-},
-{
-    img: "https://picsum.photos/400/600/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/300/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-{
-    img: "https://picsum.photos/400/700/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
-
-{
-    img: "https://picsum.photos/400/600/?grayscale",
-    description: "The wisdom of life consists in the elimination of non-essentials.",
-    link: "#",
-},
+}
 ]
 
 const contactConfig = {
